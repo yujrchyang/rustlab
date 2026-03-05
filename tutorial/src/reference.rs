a7d3216321c9adb988172f5039215d9e5cc5be97
yujrchyang <yujrchyang@gmail.com>
2026-03-05 10:33:31 +0800

    update test code for reference

--- tutorial/src/reference.rs
+++ tutorial/src/reference.rs
@@ -1,5 +1,6 @@
 pub fn run_reference() {
     test_overhanging_reference();
+    test_mutable();
 }
 
 fn test_overhanging_reference() {
@@ -15,4 +16,34 @@ fn test_overhanging_reference() {
     //     let s = String::from("hello");
     //     &s
     // }
+}
+
+fn test_mutable() {
+    let a = 1;
+    let b = 2;
+    let mut s = "hello".to_string();
+
+    let x = &a;
+    println!("x is {}", x);
+    // can't assign twice
+    // x = &b;
+    // println!("x is {}", x);
+
+    let mut y = &b;
+    println!("y is {}", y);
+    // can assign twice because y is mutable
+    y = &a;
+    println!("y is {}", y);
+
+    let z = &s;
+    println!("z is {}", z);
+    // z is immutable, so can't chang s
+    // z.push_str(" world");
+    // println!("z is {}", z);
+
+    let x = &mut s;
+    println!("x is {}", x);
+    x.push_str(" world");
+    println!("x is {}", x);
+    println!("s is {}", s);
 }
