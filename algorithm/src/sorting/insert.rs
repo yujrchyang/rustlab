65b8390fbef7382708ff58c35336ac6fb24687e8
yujrchyang <yujrchyang@gmail.com>
2026-03-08 16:03:21 +0800

    algo: add implemention and test code for insert sort

---
+++ algorithm/src/sorting/insert.rs
@@ -0,0 +1,55 @@
+pub fn insert_sort<T: Ord>(arr: &mut Vec<T>) {
+    let size = arr.len();
+    if size <= 1 {
+        return;
+    }
+
+    for i in 1..size {
+        let mut j = i;
+        while j > 0 && arr[j - 1] > arr[j] {
+            arr.swap(j - 1, j);
+            j -= 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty_vec() {
+        let mut empty_vec: Vec<String> = vec![];
+        insert_sort(&mut empty_vec);
+        assert_eq!(empty_vec, Vec::<String>::new());
+    }
+
+    #[test]
+    fn test_number_vec() {
+        let mut num_vec = vec![1, 9, 5, 7, 3];
+        insert_sort(&mut num_vec);
+        assert_eq!(num_vec, vec![1, 3, 5, 7, 9]);
+    }
+
+    #[test]
+    fn test_string_vec() {
+        let mut str_vec = vec![
+            String::from("alice"),
+            String::from("david"),
+            String::from("black"),
+            String::from("bob"),
+            String::from("carol"),
+        ];
+        insert_sort(&mut str_vec);
+        assert_eq!(
+            str_vec,
+            vec![
+                String::from("alice"),
+                String::from("black"),
+                String::from("bob"),
+                String::from("carol"),
+                String::from("david"),
+            ]
+        )
+    }
+}
