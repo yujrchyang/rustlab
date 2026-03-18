1beb21041bf268310f076f5a279505cadd2304e3
yujrchyang <yujrchyang@gmail.com>
2026-03-18 21:50:42 +0800

    bitcask: implement open database

--- bitcask/src/index/mod.rs
+++ bitcask/src/index/mod.rs
@@ -1,6 +1,6 @@
 pub mod btree;
 
-use crate::data::log_record::LogRecordPos;
+use crate::{data::log_record::LogRecordPos, options::IndexType};
 
 // Indexer 抽象索引接口，后续如果想要接入其他的数据结构，则直接实现这个接口即可
 pub trait Indexer: Sync + Send {
@@ -10,4 +10,12 @@ pub trait Indexer: Sync + Send {
     fn get(&self, key: Vec<u8>) -> Option<LogRecordPos>;
     // 根据 key 删除对应的索引位置信息
     fn delete(&self, key: Vec<u8>) -> bool;
+}
+
+// 根据类型创建内存索引
+pub fn new_indexer(index_type: IndexType) -> impl Indexer {
+    match index_type {
+        IndexType::BTree => btree::BTree::new(),
+        IndexType::SkipList => todo!(),
+    }
 }
