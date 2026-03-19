4b35feef906a7ee25e9dd41d639537f2347f3e5b
yujrchyang <yujrchyang@gmail.com>
2026-03-19 11:38:17 +0800

    refactor(algo): reimpl insertion sort and delete binary

--- algorithm/src/sorting/insertion.rs
+++ algorithm/src/sorting/insertion.rs
@@ -1,28 +1,10 @@
-pub fn insertion_sort<T: Ord>(arr: &mut Vec<T>) {
-    let size = arr.len();
-    if size <= 1 {
+pub fn insertion_sort<T: PartialOrd>(arr: &mut Vec<T>) {
+    if arr.len() <= 1 {
         return;
     }
-
-    for i in 1..size {
+    for i in 1..arr.len() {
         let mut j = i;
         while j > 0 && arr[j - 1] > arr[j] {
-            arr.swap(j - 1, j);
-            j -= 1;
-        }
-    }
-}
-
-pub fn insertion_sort_binary_search<T: Ord>(arr: &mut [T]) {
-    let size = arr.len();
-    if size <= 1 {
-        return;
-    }
-
-    for i in 1..size {
-        let pos = arr[..i].binary_search(&arr[i]).unwrap_or_else(|pos| pos);
-        let mut j = i;
-        while j > pos {
             arr.swap(j - 1, j);
             j -= 1;
         }
@@ -33,83 +15,46 @@ pub fn insertion_sort_binary_search<T: Ord>(arr: &mut [T]) {
 mod tests {
     use super::*;
 
-    mod insertion_sort {
-        use super::*;
-
-        #[test]
-        fn test_empty_vec() {
-            let mut empty_vec: Vec<String> = vec![];
-            insertion_sort(&mut empty_vec);
-            assert_eq!(empty_vec, Vec::<String>::new());
-        }
-
-        #[test]
-        fn test_number_vec() {
-            let mut num_vec = vec![1, 9, 5, 7, 3];
-            insertion_sort(&mut num_vec);
-            assert_eq!(num_vec, vec![1, 3, 5, 7, 9]);
-        }
-
-        #[test]
-        fn test_string_vec() {
-            let mut str_vec = vec![
-                String::from("alice"),
-                String::from("david"),
-                String::from("black"),
-                String::from("bob"),
-                String::from("carol"),
-            ];
-            insertion_sort(&mut str_vec);
-            assert_eq!(
-                str_vec,
-                vec![
-                    String::from("alice"),
-                    String::from("black"),
-                    String::from("bob"),
-                    String::from("carol"),
-                    String::from("david"),
-                ]
-            )
-        }
+    #[test]
+    fn test_empty_vec() {
+        let mut empty_vec: Vec<String> = vec![];
+        insertion_sort(&mut empty_vec);
+        assert_eq!(empty_vec, Vec::<String>::new());
     }
 
-    mod insertion_sort_binary_search {
-        use super::*;
+    #[test]
+    fn test_number_vec() {
+        let mut num_vec = vec![1, 9, 5, 7, 3];
+        insertion_sort(&mut num_vec);
+        assert_eq!(num_vec, vec![1, 3, 5, 7, 9]);
+    }
 
-        #[test]
-        fn test_empty_vec() {
-            let mut empty_vec: Vec<String> = vec![];
-            insertion_sort_binary_search(&mut empty_vec);
-            assert_eq!(empty_vec, Vec::<String>::new());
-        }
+    #[test]
+    fn test_float_vec() {
+        let mut num_vec = vec![1.1, 9.9, 5.5, 7.7, 3.3];
+        insertion_sort(&mut num_vec);
+        assert_eq!(num_vec, vec![1.1, 3.3, 5.5, 7.7, 9.9]);
+    }
 
-        #[test]
-        fn test_number_vec() {
-            let mut num_vec = vec![1, 9, 5, 7, 3];
-            insertion_sort_binary_search(&mut num_vec);
-            assert_eq!(num_vec, vec![1, 3, 5, 7, 9]);
-        }
-
-        #[test]
-        fn test_string_vec() {
-            let mut str_vec = vec![
+    #[test]
+    fn test_string_vec() {
+        let mut str_vec = vec![
+            String::from("alice"),
+            String::from("david"),
+            String::from("black"),
+            String::from("bob"),
+            String::from("carol"),
+        ];
+        insertion_sort(&mut str_vec);
+        assert_eq!(
+            str_vec,
+            vec![
                 String::from("alice"),
-                String::from("david"),
                 String::from("black"),
                 String::from("bob"),
                 String::from("carol"),
-            ];
-            insertion_sort_binary_search(&mut str_vec);
-            assert_eq!(
-                str_vec,
-                vec![
-                    String::from("alice"),
-                    String::from("black"),
-                    String::from("bob"),
-                    String::from("carol"),
-                    String::from("david"),
-                ]
-            )
-        }
+                String::from("david"),
+            ]
+        )
     }
 }
