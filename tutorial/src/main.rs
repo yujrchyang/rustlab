8d82e811ab6c15de9443447e859ede640c2b08ca
yujrchyang <yujrchyang@gmail.com>
2026-03-31 10:37:32 +0800

    tutorial: init crate

---
+++ tutorial/src/main.rs
@@ -0,0 +1,27 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about = "RustLab Tutorial Exercise")]
+struct Args {
+    #[arg(short, long, default_value = "hello")]
+    exercise: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    match args.exercise.as_str() {
+        "hello" => run_hello(),
+        _ => {
+            eprintln!(
+                "Error: Exercise '{}' not found. Please type --help to see available options.",
+                args.exercise
+            );
+            std::process::exit(1)
+        }
+    }
+}
+
+fn run_hello() {
+    println!("hello world!");
+}
