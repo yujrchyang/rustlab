27d05ea5a23314f3834ca0e55b1b01e5ad74bae9
yujrchyang <yujrchyang@gmail.com>
2026-04-01 16:03:17 +0800

    tutorial: add httpie example

---
+++ tutorial/src/bin/httpie.rs
@@ -0,0 +1,131 @@
+use std::{collections::HashMap, str::FromStr};
+
+use anyhow::{Ok, Result, anyhow};
+use clap::{Parser, Subcommand};
+use colored::*;
+use mime::Mime;
+use reqwest::{Client, Response, Url, header};
+
+#[derive(Debug, Parser)]
+#[command(
+    version = "1.0",
+    author = "Jared Yu <yujrchyang@gmail.com>",
+    about = "A native httpie implementation with Rust."
+)]
+struct Opts {
+    #[command(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Debug, Subcommand)]
+enum SubCommand {
+    Get(Get),
+    Post(Post),
+}
+
+#[derive(Debug, Parser)]
+struct Get {
+    #[arg()]
+    #[arg(value_parser = parse_url)]
+    url: String,
+}
+
+#[derive(Debug, Parser)]
+struct Post {
+    #[arg()]
+    #[arg(value_parser = parse_url)]
+    url: String,
+
+    #[arg()]
+    body: Vec<KvPair>,
+}
+
+#[derive(Debug, Clone)]
+struct KvPair {
+    k: String,
+    v: String,
+}
+
+impl FromStr for KvPair {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let mut split = s.split("=");
+        let err = || anyhow!("Failed to parse {}", s);
+        Ok(Self {
+            k: (split.next().ok_or_else(err)?).to_string(),
+            v: (split.next().ok_or_else(err)?).to_string(),
+        })
+    }
+}
+
+fn parse_url(s: &str) -> Result<String> {
+    let _url: Url = s.parse()?;
+    Ok(s.into())
+}
+
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+async fn post(client: Client, args: &Post) -> Result<()> {
+    let mut body = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(&pair.k, &pair.v);
+    }
+    let resp = client.post(&args.url).json(&body).send().await?;
+    Ok(print_resp(resp).await?)
+}
+
+fn print_status(resp: &Response) {
+    let status = format!("{:?} {}", resp.version(), resp.status()).blue();
+    println!("{}\n", status);
+}
+
+fn print_header(resp: &Response) {
+    for (name, value) in resp.headers() {
+        println!("{}: {:?}", name.to_string().green(), value);
+    }
+    print!("\n");
+}
+
+fn print_body(m: Option<Mime>, body: &String) {
+    match m {
+        Some(v) if v == mime::APPLICATION_JSON => {
+            println!("{}", jsonxf::pretty_print(body).unwrap().cyan())
+        }
+        _ => println!("{}", body),
+    }
+}
+
+fn get_content_type(resp: &Response) -> Option<Mime> {
+    resp.headers()
+        .get(header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
+}
+
+async fn print_resp(resp: Response) -> Result<()> {
+    print_status(&resp);
+    print_header(&resp);
+    let mime = get_content_type(&resp);
+    let body = resp.text().await?;
+    print_body(mime, &body);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let opts: Opts = Opts::parse();
+    let mut headers = header::HeaderMap::new();
+    headers.insert("X-POWERED-BY", "Rust".parse()?);
+    headers.insert(header::USER_AGENT, "Rust Httpie".parse()?);
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()?;
+    let result = match opts.subcmd {
+        SubCommand::Get(ref args) => get(client, args).await?,
+        SubCommand::Post(ref args) => post(client, args).await?,
+    };
+    Ok(result)
+}
